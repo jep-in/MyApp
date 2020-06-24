b492eeecd0f36280e56021f6e5d8c5636db213ed
jep <jep@JEP-LAP.in.oracle.com>
2020-06-24 11:51:53 +0530

    second commit

--- MyApp/src/main/java/com/ofss/MyApp/App.java
+++ MyApp/src/main/java/com/ofss/MyApp/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Calculator" );
         Calculator calc = new Calculator();
         System.out.println( "Add"+calc.add(10,20) );
     }
